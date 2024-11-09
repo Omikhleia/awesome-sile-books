ed7f59f4e4fa38f5f6087665cf53c2b77b3eb1c8
Omikhleia <didier.willis@gmail.com>
2024-11-09 15:42:49 +0100

    fix(sile-math): Add extra greek characters to the showcase variant tables

--- sile-maths/mathvariant/mathvariant-tables.lua
+++ sile-maths/mathvariant/mathvariant-tables.lua
@@ -35,13 +35,14 @@ local greek_variants = {
   "italic",
   "bold-italic",
   "bold-sans-serif",
+  "sans-serif-bold-italic",
 }
 
 local function mathvariant (variant, char)
   return ('<mathml><mo mathvariant="%s">%s</mo></mathml>'):format(variant, char)
 end
 
-local function build_table (start, stop, variants)
+local function build_table (start, stop, variants, extras)
   local ptable = {}
   local ncols = #variants
   local maxcols = #letter_variants -- The one with the most variants
@@ -70,6 +71,20 @@ local function build_table (start, stop, variants)
       table.insert(ptable, row)
     end
   end
+  if extras then
+    for _, extra in ipairs(extras) do
+      local row = {}
+      for _, variant in ipairs(variants) do
+        local cell = SU.ast.createCommand("cell", { halign = "center" }, function ()
+          SILE.call("font", { size = szfont })        
+          return SILE.processString(mathvariant(variant, luautf8.char(extra)), "xml")
+        end)
+        table.insert(row, cell)
+      end
+      local row = SU.ast.createStructuredCommand("row", {}, row)
+      table.insert(ptable, row)
+    end
+  end
   ptable = SU.ast.createStructuredCommand("ptable", {
     cols = string.rep(szcols, ncols, " "),
 --  header  = true
@@ -77,8 +92,8 @@ local function build_table (start, stop, variants)
   return ptable
 end
 
-local function do_figure (start, stop, variants, caption)
-  local ptable = build_table(start, stop, variants)
+local function do_figure (start, stop, variants, caption, extras)
+  local ptable = build_table(start, stop, variants, extras)
   local figure = SU.ast.createStructuredCommand("figure", {}, {
     SU.ast.createCommand("caption", {}, { caption }),
     ptable
@@ -98,5 +113,8 @@ do_figure(0x30, 0x30 + 9, number_variants, "MathML variants for digits")
 SILE.process({
   SU.ast.createCommand("include", { src ="mathvariant/greek.dj", shift_headings = 1 }),
 })
-do_figure(0x391, 0x391 + 24, greek_variants, "MathML variants for uppercase Greek")
-do_figure(0x3b1, 0x3b1 + 24, greek_variants, "MathML variants for lowercase Greek")
+-- Extras = capital theta symbol: U+03F4, nabla: U+2207
+do_figure(0x391, 0x391 + 24, greek_variants, "MathML variants for uppercase Greek", { 0x3F4, 0x2207} )
+-- Extras = theta variant: U+03D1, phi variant: U+03D5, pi variant: U+03D6,
+-- kappa variant: U+03F0, rho variant: U+03F1, epsilon variant: U+03F5
+do_figure(0x3b1, 0x3b1 + 24, greek_variants, "MathML variants for lowercase Greek", { 0x3D1, 0x3D5, 0x3D6, 0x3F0, 0x3F1, 0x3F5 })
